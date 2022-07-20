0ab8a09f9cdf96d131428a66873c83536fff782b
OrlandoOroporte <orlando.oroporte@gmail.com>
2022-07-20 23:22:42 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// //window.onload = function() {
+//   document.querySelector("#boton").addEventListener("click", () => {
+//     document.querySelector("#domain").innerHTML = domaingenator();
+//   });
+// };
+
+let domaingenator = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domin = [".com", ".es"];
+
+  for (let pro of pronoun) {
+    for (let ad of adj) {
+      for (let no of noun) {
+        for (let dom of domin) {
+          console.log(`${pro}${ad}${no}${dom}`);
+        }
+      }
+    }
+  }
 };
+domaingenator();
